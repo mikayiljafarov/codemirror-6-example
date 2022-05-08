2449af91def0b7eb39412951b83f236e4c4e1d2a
Vugar Dadalov <vuqar.ahmedoglu@gmail.com>
2022-05-08 13:59:00 +0400

    ref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { basicSetup } from '@codemirror/basic-setup';
 import { EditorView } from "@codemirror/view";
-import { EditorState, StateEffect } from "@codemirror/state";
+import { EditorState, StateEffect, Compartment } from "@codemirror/state";
 import { keymap, highlightActiveLine, ViewUpdate } from '@codemirror/view';
 import { sql, keywordCompletion, StandardSQL, schemaCompletion } from '@codemirror/lang-sql';
 import { indentWithTab } from '@codemirror/commands';
@@ -53,7 +53,7 @@ function getCurrentStatement(state: EditorState, from: number): ISelection | nul
 
 
 function myCompletions(context: CompletionContext): any {
-  console.clear();
+  // console.clear();
 
   const query: ISelection | null = getCurrentStatement(context.state, context.pos);
   let defaultTable = '';
@@ -114,6 +114,10 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
+    console.log(new Error('Sentry test error'));
+    throw new Error("Testetet");
+        
+    
     const self = this;
     this.view = new EditorView({
       state: EditorState.create({
@@ -239,141 +243,7 @@ export class AppComponent implements AfterViewInit {
     return true;
   }
 
+  themeToggle() {
+    
+  }
 }
-
-
-// const myHighlightStyle = HighlightStyle.define([
-//   { tag: tags.keyword, color: "#10ebd8" },
-//   { tag: tags.comment, color: "#f5d", fontStyle: "italic" }
-// ])
-
-
-// function validationErrorMarker(): any {
-//   let diagnostics = [];
-//   diagnostics.push({
-//     from: 70, to: 76,
-//     severity: "error",
-//     message: `Invalid type. Expected Integer, Null but got String.`
-//   });
-//   return diagnostics
-// }
-// // linter(validationErrorMarker),
-// // lintGutter()
-
-
-
-// findQueriesFromSelections2(view: ViewUpdate): boolean {
-//   console.clear();
-
-//   this.selection = [];
-
-//   const main = view.state.selection.main;
-
-//   if (main.from < main.to) {
-//     const from = main.from;
-//     const to = main.to;
-//     console.log(from, to);
-
-
-//     let bufferNode = syntaxTree(view.state).resolve(0, -1).firstChild;
-
-//     if (bufferNode) {
-
-//       if (bufferNode.type?.name === 'Statement') {
-//         const from = main.from <= bufferNode.from ? bufferNode.from : main.from;
-//         const to = main.to >= bufferNode.to ? bufferNode.to : main.to;
-
-//         console.log(bufferNode.type.name, bufferNode.from, bufferNode.to, '=>', from, to);
-
-//         this.selection.push({ from, to });
-//       }
-
-//       while (bufferNode?.nextSibling) {
-//         bufferNode = bufferNode.nextSibling;
-
-
-//         if (bufferNode.type.name === 'Statement') {
-//           const from = main.from <= bufferNode.from ? bufferNode.from : main.from;
-//           const to = main.to >= bufferNode.to ? bufferNode.to : main.to;
-
-//           console.log(bufferNode.type.name, bufferNode.from, bufferNode.to, '=>', from, to);
-
-//           this.selection.push({ from, to });
-//         }
-//       }
-
-//     }
-
-//     this.selection.map(s => (console.log(view.state.sliceDoc(s.from, s.to))));
-//   } else {
-//     return true;
-//   }
-//   return true;
-// }
-
-
-
-// getAllQueryFromDoc(view: ViewUpdate): boolean {
-//   console.clear();
-
-//   this.selection = [];
-
-//   let bufferNode = syntaxTree(view.state).resolve(0, -1).firstChild;
-
-//   if (bufferNode) {
-//     console.log(bufferNode.type.name, bufferNode.from, bufferNode.to);
-
-//     if (bufferNode.type?.name === 'Statement') {
-//       this.selection.push({ from: bufferNode.from, to: bufferNode.to });
-//     }
-
-//     while (bufferNode?.nextSibling) {
-//       bufferNode = bufferNode.nextSibling;
-
-//       if (bufferNode.type.name === 'Statement') {
-//         this.selection.push({ from: bufferNode.from, to: bufferNode.to });
-//       }
-//       console.log(bufferNode.type.name, bufferNode.from, bufferNode.to);
-//     }
-
-//   }
-
-//   this.selection.map(s => (console.log(s), console.log(view.state.sliceDoc(s.from, s.to))));
-
-//   return true;
-// }
-
-
-// recursive(buffer: any, main: any) {
-//   if (buffer?.type?.name === 'Statement') {
-//     const from = main.from <= buffer.from ? buffer.from : main.from;
-//     const to = main.to >= buffer.to ? buffer.to : main.to;
-
-//     // console.log(buffer.type.name, buffer.from, buffer.to, '=>', from, to);
-
-//     if (from < to) {
-//       this.selection.push({ from, to });
-//     }
-//   }
-
-//   if (buffer?.nextSibling) {
-//     this.recursive(buffer.nextSibling, main);
-//   }
-// }
-
-
-
-
-// changeDoc() {
-//   // console.log(this.view.state);
-//   this.view.dispatch({
-//     changes: { from: 0, insert: "#!/usr/bin/env node\n" }
-//   })
-
-//   // this.view.dispatch({
-//   //   effects: StateEffect.appendConfig.of(extension)
-//   // })
-// }
-
-
-  // let node = context.tokenBefore(['(', 'Identifier']);
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -19,3 +19,141 @@ export class AppService {
     return this.http.get<any>(url).pipe(map(() => ['table1', 'table2']));
   }
 }
+
+
+
+// const myHighlightStyle = HighlightStyle.define([
+//   { tag: tags.keyword, color: "#10ebd8" },
+//   { tag: tags.comment, color: "#f5d", fontStyle: "italic" }
+// ])
+
+
+// function validationErrorMarker(): any {
+//   let diagnostics = [];
+//   diagnostics.push({
+//     from: 70, to: 76,
+//     severity: "error",
+//     message: `Invalid type. Expected Integer, Null but got String.`
+//   });
+//   return diagnostics
+// }
+// // linter(validationErrorMarker),
+// // lintGutter()
+
+
+
+// findQueriesFromSelections2(view: ViewUpdate): boolean {
+//   console.clear();
+
+//   this.selection = [];
+
+//   const main = view.state.selection.main;
+
+//   if (main.from < main.to) {
+//     const from = main.from;
+//     const to = main.to;
+//     console.log(from, to);
+
+
+//     let bufferNode = syntaxTree(view.state).resolve(0, -1).firstChild;
+
+//     if (bufferNode) {
+
+//       if (bufferNode.type?.name === 'Statement') {
+//         const from = main.from <= bufferNode.from ? bufferNode.from : main.from;
+//         const to = main.to >= bufferNode.to ? bufferNode.to : main.to;
+
+//         console.log(bufferNode.type.name, bufferNode.from, bufferNode.to, '=>', from, to);
+
+//         this.selection.push({ from, to });
+//       }
+
+//       while (bufferNode?.nextSibling) {
+//         bufferNode = bufferNode.nextSibling;
+
+
+//         if (bufferNode.type.name === 'Statement') {
+//           const from = main.from <= bufferNode.from ? bufferNode.from : main.from;
+//           const to = main.to >= bufferNode.to ? bufferNode.to : main.to;
+
+//           console.log(bufferNode.type.name, bufferNode.from, bufferNode.to, '=>', from, to);
+
+//           this.selection.push({ from, to });
+//         }
+//       }
+
+//     }
+
+//     this.selection.map(s => (console.log(view.state.sliceDoc(s.from, s.to))));
+//   } else {
+//     return true;
+//   }
+//   return true;
+// }
+
+
+
+// getAllQueryFromDoc(view: ViewUpdate): boolean {
+//   console.clear();
+
+//   this.selection = [];
+
+//   let bufferNode = syntaxTree(view.state).resolve(0, -1).firstChild;
+
+//   if (bufferNode) {
+//     console.log(bufferNode.type.name, bufferNode.from, bufferNode.to);
+
+//     if (bufferNode.type?.name === 'Statement') {
+//       this.selection.push({ from: bufferNode.from, to: bufferNode.to });
+//     }
+
+//     while (bufferNode?.nextSibling) {
+//       bufferNode = bufferNode.nextSibling;
+
+//       if (bufferNode.type.name === 'Statement') {
+//         this.selection.push({ from: bufferNode.from, to: bufferNode.to });
+//       }
+//       console.log(bufferNode.type.name, bufferNode.from, bufferNode.to);
+//     }
+
+//   }
+
+//   this.selection.map(s => (console.log(s), console.log(view.state.sliceDoc(s.from, s.to))));
+
+//   return true;
+// }
+
+
+// recursive(buffer: any, main: any) {
+//   if (buffer?.type?.name === 'Statement') {
+//     const from = main.from <= buffer.from ? buffer.from : main.from;
+//     const to = main.to >= buffer.to ? buffer.to : main.to;
+
+//     // console.log(buffer.type.name, buffer.from, buffer.to, '=>', from, to);
+
+//     if (from < to) {
+//       this.selection.push({ from, to });
+//     }
+//   }
+
+//   if (buffer?.nextSibling) {
+//     this.recursive(buffer.nextSibling, main);
+//   }
+// }
+
+
+
+
+// changeDoc() {
+//   // console.log(this.view.state);
+//   this.view.dispatch({
+//     changes: { from: 0, insert: "#!/usr/bin/env node\n" }
+//   })
+
+//   // this.view.dispatch({
+//   //   effects: StateEffect.appendConfig.of(extension)
+//   // })
+// }
+
+
+  // let node = context.tokenBefore(['(', 'Identifier']);
